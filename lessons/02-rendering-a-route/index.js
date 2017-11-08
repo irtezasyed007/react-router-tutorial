cf707ea083ac248297fc390ea2c0a48bf1472135
Irteza Syed <irteza.m.syed@gmail.com>
2017-11-08 17:06:08 -0500

    finished tutorial (most of it)

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,24 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import React from 'react';
+import { render } from 'react-dom';
+import App from './modules/App';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'; 
+// insert into index.js
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+
+      {/* add it here, as a child of `/` */}
+      <IndexRoute component={Home}/>
+
+      <Route path="/repos" component={Repos}>
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
